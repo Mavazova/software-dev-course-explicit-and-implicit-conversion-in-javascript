efc5559780bfd0cd4b5479a7dbdabb44c6db976e
Shakhruh Hasanov <shakhruhhasanov@shakhruhs-air.mynetworksettings.com>
2025-03-05 16:29:26 -0600

    Lesson 3

--- Main.js
+++ Main.js
@@ -19,14 +19,18 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = "5" - 2;//this one example of implicit type conversion 
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false" === "true");//this also explicit conversion. boolean method onlu converts empty, null,Nan, 0 to false. It does not convert string
+//console.log(isValid);
 if (isValid) {
-    console.log("This is valid!");
+    console.log("This is valid!");//this should not run
+}
+else{
+  console.log("This is not valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;//this is explicit type conversion
 console.log("Total Age: " + totalAge);
